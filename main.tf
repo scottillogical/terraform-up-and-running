d237a6b22ba7601de8b5847845c3be9fbf3b38e2
Scott Schulthess <sschulthess@zipcar.com>
2020-02-17 19:21:51 -0500

    adding clusters

--- main.tf
+++ main.tf
@@ -2,27 +2,60 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+variable "server_port" {
+  description = "The port to use for http comms"
+  type = number
+  default = 8080
+}
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0c55b159cbfafe1f0"
+  instance_type  = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} 8080 &
               EOF
 
-  tags = {
-    Name = "terraform-example"
+  # Required when using a launch configuration with an autoscaling roup
+  lifecycle {
+    create_before_destroy = true
   }
+
 }
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+
+}
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+  min_size = 2
+  max_size = 20
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
+}
+
+#output "public_ip" {
+  #value = aws_launch_configuration.example.public_dns
+  #description = "The public IP of the webserver"
+#}
