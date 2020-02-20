0ad2d618a3ecd788951c7839c699d47dd76d4ae9
Scott Schulthess <sschulthess@zipcar.com>
2020-02-20 10:41:29 -0500

    works over http

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = "us-east-2"
 }
-
 variable "server_port" {
   description = "The port to use for http comms"
   type = number
@@ -23,6 +22,22 @@ resource "aws_lb" "example" {
   security_groups = [aws_security_group.alb.id]
 }
 
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 16
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
 resource "aws_launch_configuration" "example" {
   image_id = "ami-0c55b159cbfafe1f0"
   instance_type  = "t2.micro"
@@ -37,12 +52,13 @@ resource "aws_launch_configuration" "example" {
   lifecycle {
     create_before_destroy = true
   }
-
 }
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
     from_port = var.server_port
+
     to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
@@ -52,6 +68,8 @@ resource "aws_security_group" "instance" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnet_ids.default.ids
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
   min_size = 2
   max_size = 20
   tag {
@@ -92,24 +110,21 @@ resource "aws_security_group" "alb" {
    }
 }
 
-resource "aws_lb_target_group" "asg" {
-  name = "terraform-asg-example"
-  port = var.server_port
-  protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
-
-  health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-    interval = 16
-    timeout = 3
-    healthy_threshold = 2
-    unhealthy_threshold = 2
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+  condition {
+    path_pattern {
+      values=["*"]
+    }
+  }
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
 
-#"output "public_ip" {
-  #value = aws_launch_configuration.example.public_dns
-  #description = "The public IP of the webserver"
-#}
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
